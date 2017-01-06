2984443896f8ff8aa973f837c691bcb732a5e2dc
chrisruk <chrisrichardsonuk@gmail.com>
2017-01-06 13:46:35 +0000

    producing accuracy output

--- cnn_generate.py
+++ cnn_generate.py
@@ -15,6 +15,8 @@ import struct
 import os
 import keras
 
+import matplotlib.pyplot as plt
+
 from keras.optimizers  import Adam
 from keras.constraints import MaxNorm
 
@@ -38,6 +40,16 @@ from data_generate import *
 
 np.set_printoptions(threshold=np.nan)
 
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, labels=[]):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation=45)
+    plt.yticks(tick_marks, labels)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 def to_onehot(yy):
     yy1 = np.zeros([len(yy), max(yy)+1])
@@ -71,7 +83,7 @@ def loadRadio():
     Y_train = to_onehot(map(lambda x: mods.index(lbl[x][0]), train_idx))
     Y_test = to_onehot(map(lambda x: mods.index(lbl[x][0]), test_idx))
 
-    return X_train,Y_train,X_test,Y_test,mods,snrs
+    return X_train,Y_train,X_test,Y_test,mods,snrs,train_idx,test_idx,lbl
 
 
 ## Handles flow graph for CNN
@@ -224,8 +236,8 @@ def process(train, m, sn, z, qu, sym):
 ## \param test_i Testing data
 ## \param test_o Class for each testing item
 ## \param mod List of modulation schemes
-def cnn(train_i, train_o, test_i, test_o,mods,snrs):
-    
+def cnn(train_i, train_o, test_i, test_o,mods,snrs,train_idx,test_idx,lbl):
+ 
     # CNN1
     c1 = 64
     c2 = 16
@@ -307,7 +319,7 @@ def cnn(train_i, train_o, test_i, test_o,mods,snrs):
             test_o[0]))
     """
     # Set up some params 
-    nb_epoch = 100     # number of epochs to train on
+    nb_epoch = 2  #100   # number of epochs to train on
     batch_size = 1024  # training batch size
 
     tb = TensorBoard(log_dir='./logs')
@@ -329,24 +341,58 @@ def cnn(train_i, train_o, test_i, test_o,mods,snrs):
     # we re-load the best weights once training is finished
     model.load_weights(filepath)
 
-    for s in sorted(test_i):
-        X_test = np.array(test_i[s])
-        Y_test = np.array(test_o[s])
-        score = model.evaluate(X_test, Y_test, verbose=0)
-        print("SNR", s, "Test accuracy:", score[1])
-
     K.set_learning_phase(0)
+
+
+
+    acc = {}
+    for snr in snrs:
+
+        # extract classes @ SNR
+        test_SNRs = map(lambda x: lbl[x][1], test_idx)
+        test_X_i = X_test[np.where(np.array(test_SNRs)==snr)]
+        test_Y_i = Y_test[np.where(np.array(test_SNRs)==snr)]    
+
+        # estimate classes
+        test_Y_i_hat = model.predict(test_X_i)
+        conf = np.zeros([len(classes),len(classes)])
+        confnorm = np.zeros([len(classes),len(classes)])
+        for i in range(0,test_X_i.shape[0]):
+            j = list(test_Y_i[i,:]).index(1)
+            k = int(np.argmax(test_Y_i_hat[i,:]))
+            conf[j,k] = conf[j,k] + 1
+        for i in range(0,len(classes)):
+            confnorm[i,:] = conf[i,:] / np.sum(conf[i,:])
+        plt.figure()
+        plot_confusion_matrix(confnorm, labels=classes, title="ConvNet Confusion Matrix (SNR=%d)"%(snr))
+    
+        cor = np.sum(np.diag(conf))
+        ncor = np.sum(conf) - cor
+        print ("Overall Accuracy: ", cor / (cor+ncor))
+        acc[snr] = 1.0*cor/(cor+ncor)
+
+    
+
+
+
+
+
+
+
+
+
+
 
     config = model.get_config()
     weights = model.get_weights()
 
-    new_model = Sequential.from_config(config)
+    new_model = models.Sequential.from_config(config)
     new_model.set_weights(weights)
 
     export_path = "/tmp/cnn"
     export_version = 1
 
-    labels_tensor = tf.constant(mod)
+    labels_tensor = tf.constant(mods)
 
     saver = tf.train.Saver(sharded=True)
     model_exporter = exporter.Exporter(saver)
@@ -366,6 +412,6 @@ if __name__ == '__main__':
     train_i, train_o = getdata(range(9), [3,4], process, True)
     """
 
-    X_train,Y_train,X_test,Y_test,mods,snrs = loadRadio()
+    X_train,Y_train,X_test,Y_test,mods,snrs,train_idx,test_idx,lbl = loadRadio()
     
-    cnn(X_train,Y_train,X_test,Y_test,mods,snrs)
+    cnn(X_train,Y_train,X_test,Y_test,mods,snrs,train_idx,test_idx,lbl)
